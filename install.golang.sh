59e0bccf263d98ecf1e5897a012e633ec4e36edd
ali <eshax@qq.com>
2021-07-07 07:57:07 +0000

    .

--- install.golang.sh
+++ install.golang.sh
@@ -6,3 +6,5 @@ cd ~
 wget https://golang.google.cn/dl/go$ver.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go$ver.linux-amd64.tar.gz
 ln -s /usr/local/go/bin/go /usr/bin/go
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,direct
